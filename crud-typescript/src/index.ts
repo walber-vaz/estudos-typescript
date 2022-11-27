7f7c3beb7c4c2a8446e10daeecc7a039551271f2
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-11-27 18:54:35 -0300

    Implementando error not-found e not-found-middleware

--- crud-typescript/src/index.ts
+++ crud-typescript/src/index.ts
@@ -2,7 +2,10 @@ import cors from 'cors';
 import * as dotenv from 'dotenv';
 import express from 'express';
 import helmet from 'helmet';
+
 import { itemsRouter } from './items/items.router';
+import { errorHandle } from './middleware/error.middleware';
+import { notFoundHandler } from './middleware/not-found.middleware';
 
 dotenv.config();
 
@@ -18,6 +21,9 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use('/items', itemsRouter);
+
+app.use(notFoundHandler);
+app.use(errorHandle);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
