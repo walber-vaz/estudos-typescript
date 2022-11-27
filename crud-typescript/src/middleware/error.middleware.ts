fe3d0fc121013539f8e758cfd8fb46662ecf86ab
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-11-27 18:47:03 -0300

    Implementando Error Middleware

---
+++ crud-typescript/src/middleware/error.middleware.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from 'express';
+import HttpException from '../common/http-exception';
+
+export const errorHandle = (
+  error: HttpException,
+  request: Request,
+  response: Response,
+  next: NextFunction) => {
+  const status = error.statusCode || error.status || 500;
+  response.status(status).send(error.message);
+};
